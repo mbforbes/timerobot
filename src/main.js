37b33c9900b349ed130c074de3ead1463db49b0c
Max Forbes <maxforbes@google.com>
2014-11-22 02:24:54 -0800

    Background and items.

--- src/main.js
+++ src/main.js
@@ -1,16 +1,31 @@
 // consts
-var WIDTH = 800;
-var HEIGHT = 600;
+var STAGE_WIDTH = 800;
+var STAGE_HEIGHT = 600;
+
+var WORLD_WIDTH = 1779;
+var WORLD_HEIGHT = STAGE_HEIGHT;
+
 var GROUND_HEIGHT = 200;
-var Y_GROUND = HEIGHT - GROUND_HEIGHT;
+var Y_GROUND = STAGE_HEIGHT - GROUND_HEIGHT;
+
 var ROBOT_WIDTH = 50;
 var ROBOT_HEIGHT = 54;
-var ROBOT_SPEED = 200;
+var ROBOT_SPEED = 150;
+
+var PLANK_X = 100;
+var PLANK_W = 50;
+var POISON_X = 200;
+var POISON_W = 50;
+var CAKE_X = 400;
+var CAKE_W = 300;
+var BEES_X = 700;
+var BEES_W = 100;
 
 // globals
-var robot;
+var robot, plank, poison, cake, bees;
 var game = new Phaser.Game(
-	WIDTH, HEIGHT,
+	STAGE_WIDTH,
+	STAGE_HEIGHT,
 	Phaser.CANVAS,
 	'phaser-example',
 	{
@@ -71,9 +86,34 @@ Inventory.prototype = {
 
 function preload() {
 	game.load.spritesheet('robotsprite', 'assets/sprites/robot_ss.png', ROBOT_WIDTH, ROBOT_HEIGHT);
+	game.load.image('background', 'assets/backgrounds/bare_stage.png');
+	game.load.image('rain', 'assets/sprites/rain.png');
+	game.load.image('plank', 'assets/sprites/plank_item.png');
+	game.load.image('cake', 'assets/sprites/cake_item.png');
+	game.load.image('bees', 'assets/sprites/bees_item.png');
+	game.load.image('poison', 'assets/sprites/poison_item.png');
 }
 
 function create() {
+	// game poperties
+	game.stage.backgroundColor = '#bed0ee';
+	game.world.setBounds(0, 0, WORLD_WIDTH, WORLD_HEIGHT);
+	bg = game.add.tileSprite(0, 0, WORLD_WIDTH, WORLD_HEIGHT, 'background');
+
+	// rain
+	var emitter = game.add.emitter(game.world.centerX, 0, 400);
+	emitter.width = game.world.width;
+	emitter.angle = 30; // uncomment to set an angle for the rain.
+	emitter.makeParticles('rain');
+	emitter.minParticleScale = 0.1;
+	emitter.maxParticleScale = 0.5;
+	emitter.setYSpeed(300, 500);
+	emitter.setXSpeed(-5, 5);
+	emitter.minRotation = 0;
+	emitter.maxRotation = 0;
+	emitter.start(false, 1600, 5, 0);
+
+	// the robot
 	robot = new Robot();
 	robot.sprite = game.add.sprite(0, Y_GROUND - ROBOT_HEIGHT, 'robotsprite');
 	robot.sprite.animations.add('left', [3, 4, 3, 5], 10, true);
@@ -91,6 +131,28 @@ function create() {
 
 	// defaults
 	robot.sprite.animations.frame = 3; // left
+
+	// place objects
+	plank = game.add.sprite(PLANK_X, Y_GROUND, 'plank');
+	plank_scale = PLANK_W / plank.width;
+	plank.scale.x = plank_scale;
+	plank.scale.y = plank_scale;
+
+	poison = game.add.sprite(POISON_X, Y_GROUND, 'poison');
+	poison_scale = POISON_W / poison.width;
+	poison.scale.x = poison_scale;
+	poison.scale.y = poison_scale;
+
+	cake = game.add.sprite(CAKE_X, Y_GROUND, 'cake');
+	cake_scale = CAKE_W / cake.width;
+	cake.scale.x = cake_scale;
+	cake.scale.y = cake_scale;
+	cake.y = Y_GROUND - 150; // TODO(mbforbes) Change.
+
+	bees = game.add.sprite(BEES_X, Y_GROUND, 'bees');
+	bees_scale = BEES_W / bees.width;
+	bees.scale.x = bees_scale;
+	bees.scale.y = bees_scale;
 }
 
 function update() {
